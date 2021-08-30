19e4fb52eb6fc50f372ce7b6c8d2c756dd953401
Subhakanta <subhagit47@gmail.com>
2021-08-30 22:11:50 +0530

    Added movie recommendation

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import config
 import torch
 from flask import Flask, request
 from model import BERTBaseUncased
+import scrape_reviews
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ MODEL = None
 DEVICE = config.DEVICE
 
 
-def sentence_prediction(sentence):
+def get_sentiment(sentence):
     tokenizer = config.TOKENIZER
     max_len = config.MAX_LEN
     sentence = str(sentence)
@@ -31,13 +32,37 @@ def sentence_prediction(sentence):
     return outputs
 
 
-@app.route("/predict")
+@app.route("/")
 def predict():
+    movie_name = request.args.get("movie_name")
+    review_list = scrape_reviews.get_reviews(movie_name)
+    pos_list = []
+    neg_list = []
+    for sentence in review_list:
+        prediction = get_sentiment(sentence)
+        positive_prediction = prediction
+        negative_prediction = 1 - positive_prediction
+        pos_list.append(positive_prediction)
+        neg_list.append(negative_prediction)
+    positive_score = sum(pos_list) / len(pos_list)
+    negative_score = sum(neg_list) / len(neg_list)
+    response = {
+        "Verdict": "Positive" if positive_score > negative_score else "Negative",
+        "movie_name": str(movie_name),
+        "negative": str(negative_score),
+        "positive": str(positive_score),
+    }
+    return flask.jsonify(response)
+
+
+@app.route("/predict")
+def predict_sentence():
     sentence = request.args.get("sentence")
-    positive_prediction = sentence_prediction(sentence)
+    positive_prediction = get_sentiment(sentence)
     negative_prediction = 1 - positive_prediction
     print(sentence)
     response = {
+        "Verdict": "Positive" if positive_prediction > negative_prediction else "Negative",
         "positive": str(positive_prediction),
         "negative": str(negative_prediction),
         "sentence": str(sentence),
@@ -47,6 +72,6 @@ def predict():
 
 if __name__ == "__main__":
     MODEL = BERTBaseUncased().to(DEVICE)
-    MODEL.load_state_dict(torch.load(config.MODEL_PATH + "/model.bin"))
+    MODEL.load_state_dict(torch.load(config.MODEL_PATH + "bert_model_2.bin"))
     MODEL.eval()
     app.run(debug=True)
